434914154f220c31986a248aa6414c21d619f2a5
Richard Gross <richard.gross@maibornwolff.de>
2019-05-08 15:14:04 +0200

    initial commit

---
+++ src/main/kotlin/de/richargh/sandbox/Mojo.kt
@@ -0,0 +1,11 @@
+package de.richargh.sandbox
+
+class Mojo{
+    init {
+        println("Doooo")
+    }
+}
+
+fun main(args: Array<String>){
+    Mojo()
+}
---
+++ src/test/kotlin/de/richargh/sandbox/MojoTest.kt
@@ -0,0 +1,12 @@
+package de.richargh.sandbox
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class MojoTest{
+
+    @Test
+    fun `test constructor`(){
+        Mojo()
+    }
+}
