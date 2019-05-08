abd11c76be42531fdd1356ce1a142d670eaa6415
Richard Gross <richard.gross@maibornwolff.de>
2019-05-08 23:16:20 +0200

    reduced plugin to it's most basic form

--- simple-maven-plugin/src/main/kotlin/de/richargh/sandbox/BuildMojo.kt
+++ simple-maven-plugin/src/main/kotlin/de/richargh/sandbox/BuildMojo.kt
@@ -2,14 +2,13 @@ package de.richargh.sandbox
 
 import org.apache.maven.plugin.AbstractMojo
 import org.apache.maven.plugin.MojoExecutionException
-import org.apache.maven.plugins.annotations.LifecyclePhase
 import org.apache.maven.plugins.annotations.Mojo
 
-@Mojo(name = "sayhi", defaultPhase = LifecyclePhase.COMPILE)
-class BuildMojo(): AbstractMojo() {
+@Mojo(name = "sayhi")
+class BuildMojo: AbstractMojo() {
 
     @Throws(MojoExecutionException::class)
     override fun execute() {
-        log.info("Hello, world!");
+        log.info("Hi, world!");
     }
 }
